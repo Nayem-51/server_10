5266ce0f1e64aea092ece5a3be3434381f102542
Nayem-51 <nayem20talukdar@gmail.com>
2025-11-11 18:53:24 +0600

    package.json

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 3000;
-app.get('/',(req,res)=>{
-    res.send('hello world')
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
-const users=[
-    {id :1 ,name :'Nayem',email :'a@gmail.com'},
-     {id :2 ,name :'bayem',email :'b@gmail.com'},
-     {id :3 ,name :'cayem',email :'c@gmail.com'},
-     
-]
-app.use(cors());
-app.get('/users',(req,res)=>{
-    res.send(users);
-})
-app.listen(port,()=>{
-    console.log(`server is running : ${port}`)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
