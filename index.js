a8b83e134d1cb05e81a6a5fb0dd4441f51690557
Nayem-51 <nayem20talukdar@gmail.com>
2025-11-11 19:11:55 +0600

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port =Process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('SERVERIS RUNNING ');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
